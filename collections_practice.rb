f333a4b1eb9ec8e67279abb3df0eff04cccb8262
Aubree Abril <aubree.abril@gmail.com>
2018-06-30 00:48:47 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,3 +32,15 @@ def remove_non_strings(array)
     end
     all_strings
   end
+
+def count_elements(array)
+  array.each do |original_hash|
+    original_hash[:count] = 0
+    name = original_hash[:name]
+    array.each do |hash|
+      if hash[:name] == name
+        original_hash[:count] += 1
+      end
+    end
+  end.uniq
+end
